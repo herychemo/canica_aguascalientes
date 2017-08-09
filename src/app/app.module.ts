ed179deadb26996f278171372e92fc078cc7f220
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-08-09 14:56:16 -0500

    base project x2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/** Routes */
+import {APP_ROUTING} from './app.routes';
+
+/** Services */
+
+
+/** Components */
 import { AppComponent } from './app.component';
+
+
+/** Gallery things */
+import 'hammerjs';
+import 'mousetrap';
+import {ModalGalleryModule} from 'angular-modal-gallery';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ModalGalleryModule.forRoot(), // for gallery
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
