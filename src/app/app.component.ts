3c2bd9f9fb9147eb01109ab91093dee22543f98f
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-08-10 18:10:35 -0500

    page scrolls top on router activate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'canica';
+  onActivate(e, outlet){
+      outlet.scrollTop = 0;     // Recomended, but i did not work for me            || For scroll Outlet
+      window.scrollTo(0, 0);    // Did not recomended at all, but it worked for me! || For Scroll Entire Page
+  }
 }
